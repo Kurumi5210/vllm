6c7fdaa9178e523da09165d8a598b90b1573d65a
wangxiaochao6 <wangxiaochao6@hisilicon.com>
2026-03-28 11:37:40 +0800

    fix mooncake_connect in P and D node

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -959,7 +959,7 @@ class EngineArgs:
 
         # dp_per_domain
         parallel_group.add_argument(
-            "--dp-per-domain", 
+            "--dp-per-domain",
             "-dpd",
             **parallel_kwargs["dp_per_domain"]
         )
@@ -2157,6 +2157,7 @@ class EngineArgs:
                 "disabling it for V1 backend."
             )
             self.enable_prefix_caching = False
+        self.enable_chunked_prefill = False   # TODO(wangxiaochao): need to support chunkPrefill
 
     def _set_default_max_num_seqs_and_batched_tokens_args(
         self,
